968ae1ab0c3aab96423933aec18b8bae6960382c
rishabhsharmaai <rishabh.sharma@aayaninfotech.com>
2025-03-24 15:36:19 +0530

    Fix the Error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHanlder(async (req, res, next) => {
    try {
 
-      const token = req.cookies?.accessToken || req.header("Authrization")?.replace("Bearer ", "");
+      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
       if (!token) {
          throw new ApiError(401, "Invalid Accesss Token");
