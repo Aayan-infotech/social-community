48789758caf9b5508f635bc60cd1da6db7558880
Ankul Yadav <rishabh.sharma@aayaninfotech.com>
2025-03-27 16:19:27 +0530

    Added the file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,21 @@
 import admin from "firebase-admin";
+import fs from "fs";
+import { loadConfig } from "./loadConfig";
 
-const serviceAccount =
-  typeof process.env.FIREBASE_CONFIG === "string"
-    ? JSON.parse(process.env.FIREBASE_CONFIG)
-    : process.env.FIREBASE_CONFIG;
+const config = loadConfig();
+
+let serviceAccount;
+if (process.env.NODE_ENV === "development") {
+  serviceAccount = JSON.parse(fs.readFileSync("./src/config/sericeAccountJSON.json"));
+} else {
+  serviceAccount =
+    typeof config.FIREBASE_CONFIG === "string"
+      ? JSON.parse(config.FIREBASE_CONFIG)
+      : config.FIREBASE_CONFIG;
+}
 
 const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export { firebaseAdmin };
